8a519891b469c721a8083674dd91eb2961f7f493
Dmytro Huriev <hooch.inbox@gmail.com>
2023-08-18 01:56:49 +0300

    patten

--- src/js/modal-order-now.js
+++ src/js/modal-order-now.js
@@ -5,17 +5,26 @@ const elements = {
   shoppingCartBtnEl: document.querySelector('.shopping-cart'),
   backdropEl: document.querySelector('.backdrop'),
   bodyEL: document.querySelector('body'),
+  telInputEl: document.querySelector('.tel-input'),
 };
 
 elements.heroOrderBtnEl.addEventListener('click', orderNowOpn);
 elements.shoppingCartBtnEl.addEventListener('click', orderNowOpn);
 
 function orderNowOpn(e) {
+  window.onclick = function (event) {
+    if (event.target != elements.backdropEl) {
+    } else {
+      modalClose();
+    }
+  };
   elements.bodyEL.classList.add('my-body-noscroll-class');
   elements.heroOrderBtnEl.removeEventListener('click', orderNowOpn);
   elements.shoppingCartBtnEl.removeEventListener('click', orderNowOpn);
   elements.modalCloseBtnEl.addEventListener('click', modalClose);
   elements.backdropEl.classList.remove('is-hidden');
+
+  elements.telInputEl.addEventListener('click', autofillTel);
 }
 
 function modalClose(e) {
@@ -24,4 +33,10 @@ function modalClose(e) {
   elements.heroOrderBtnEl.addEventListener('click', orderNowOpn);
   elements.shoppingCartBtnEl.addEventListener('click', orderNowOpn);
   elements.backdropEl.classList.add('is-hidden');
+}
+
+function autofillTel() {
+  if (!elements.telInputEl.value) {
+    elements.telInputEl.value = '+38';
+  }
 }
