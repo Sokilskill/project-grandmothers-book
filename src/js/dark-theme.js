2f1a422c739c909ddca381b4ecb9c41e76035275
TymQa <125615116+TymQa@users.noreply.github.com>
2023-08-19 12:15:49 +0300

    Поменял скрипт
    Поменял скрипт, добавил стили в тёмном режиме

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,10 +1,27 @@
 function darkmode() {
   const body = document.body;
-  const wasDarkMode = localStorage.getItem('darkmode') === 'true';
-
-  localStorage.setItem('darkmode', !wasDarkMode);
-  body.classList.toggle('light-theme', wasDarkMode);
-  body.classList.toggle('dark-theme', !wasDarkMode);
+  const switchElement = document.querySelector('#switch');
+  
+  if (switchElement.checked) {
+    localStorage.setItem('darkmode', 'true');
+    body.classList.add('dark-theme');
+    body.classList.remove('light-theme');
+  } else {
+    localStorage.setItem('darkmode', 'false');
+    body.classList.add('light-theme');
+    body.classList.remove('dark-theme');
+  }
 }
 
-document.querySelector('#switch').addEventListener('click', darkmode);
+document.addEventListener('DOMContentLoaded', () => {
+  const switchElement = document.querySelector('#switch');
+  const wasDarkMode = localStorage.getItem('darkmode') === 'true';
+  
+  if (wasDarkMode) {
+    switchElement.checked = true;
+  }
+  
+  darkmode(); // Apply the initial theme
+  
+  switchElement.addEventListener('click', darkmode);
+});
