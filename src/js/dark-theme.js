11dc29af195f1973280023cef2b6afea2a98550c
Sokilskill <vprovorotov@gmail.com>
2023-08-19 21:40:48 +0300

    my-var

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,27 +1,37 @@
+const switchElement = document.querySelector('#switch');
+const switchElement2 = document.querySelector('#switch2');
+const body = document.querySelector('body');
+
 function darkmode() {
-  const body = document.body;
-  const switchElement = document.querySelector('#switch');
-  
-  if (switchElement.checked) {
+  if (switchElement.checked || switchElement.checked2) {
     localStorage.setItem('darkmode', 'true');
     body.classList.add('dark-theme');
-    body.classList.remove('light-theme');
   } else {
     localStorage.setItem('darkmode', 'false');
-    body.classList.add('light-theme');
     body.classList.remove('dark-theme');
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const switchElement = document.querySelector('#switch');
   const wasDarkMode = localStorage.getItem('darkmode') === 'true';
-  
+
   if (wasDarkMode) {
     switchElement.checked = true;
+    switchElement2.checked = true;
   }
-  
+
   darkmode(); // Apply the initial theme
-  
-  switchElement.addEventListener('click', darkmode);
+
+  switchElement.addEventListener('click', function () {
+    switchElement2.checked = switchElement.checked;
+    darkmode();
+  });
+
+  switchElement.checked = switchElement2.checked;
+
+  switchElement2.addEventListener('click', function () {
+    switchElement.checked = switchElement2.checked;
+
+    darkmode();
+  });
 });
