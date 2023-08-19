727eb85bde7725c7622fbcdf4feb31745b31e982
TymQa <125615116+TymQa@users.noreply.github.com>
2023-08-19 22:11:41 +0300

    Bug fix
    Исправили баг в моб версии кнопки
    Дописал скрипт чтобы лого менялся для тёмного режима и поменял цвет фона в бургер меня
    Оптимизировал dark-theme.css

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,27 +1,41 @@
+const switchElement = document.querySelector('#switch');
+const switchElement2 = document.querySelector('#switch2');
+const body = document.querySelector('body');
+const logoImg = document.getElementById('logo-img');
+
 function darkmode() {
-  const body = document.body;
-  const switchElement = document.querySelector('#switch');
-  
-  if (switchElement.checked) {
+  if (switchElement.checked || switchElement.checked2) {
     localStorage.setItem('darkmode', 'true');
     body.classList.add('dark-theme');
-    body.classList.remove('light-theme');
+    logoImg.src = './img/header/logo-dark-theme.svg';
   } else {
     localStorage.setItem('darkmode', 'false');
-    body.classList.add('light-theme');
     body.classList.remove('dark-theme');
+    logoImg.src = './img/header/logo.svg';
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const switchElement = document.querySelector('#switch');
   const wasDarkMode = localStorage.getItem('darkmode') === 'true';
-  
+
   if (wasDarkMode) {
     switchElement.checked = true;
+    switchElement2.checked = true;
   }
-  
-  darkmode(); // Apply the initial theme
-  
-  switchElement.addEventListener('click', darkmode);
+
+  darkmode();
+
+  switchElement.addEventListener('click', function () {
+    switchElement2.checked = switchElement.checked;
+    darkmode();
+  });
+
+  switchElement.checked = switchElement2.checked;
+
+  switchElement2.addEventListener('click', function () {
+    switchElement.checked = switchElement2.checked;
+
+    darkmode();
+  });
 });
+
