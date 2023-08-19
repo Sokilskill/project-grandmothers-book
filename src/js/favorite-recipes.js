97e53dcd6ba2ac7b5eeab04013bb47f3ddf61f38
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-08-19 12:35:14 +0300

    start in favorite recipe 3

---
+++ src/js/favorite-recipes.js
@@ -0,0 +1,24 @@
+const recipe_list = document.querySelector('.popular-recipe-list');
+
+fetch("https://tasty-treats-backend.p.goit.global/api/recipes/popular")
+  .then(response => {
+
+    if (!response.ok) {
+      throw new Error(`Ошибка запроса: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    const recipe = data.map((data) => {
+        return `<li class="popular-recipe-list-li">
+        <div class="favorite-list-img">
+        <img class="round-img-1" src="${data.preview}" alt="${data.title}">
+      </div>
+        <div class="popular-recipe-div-list">
+          <h2 class="favorite-list-h2">${data.title}</h2>
+          <h3 class="favorite-list-h3" >${data.description}</h3>
+        </div>
+        </li>`;
+    });
+    recipe_list.insertAdjacentHTML('beforeend', recipe.join(''));
+  });
