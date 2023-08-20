2ba3e3810dbc3767ca63211e33abaa8518cffee8
Sokilskill <vprovorotov@gmail.com>
2023-08-21 01:45:37 +0300

    fix/dark

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -2,21 +2,24 @@ const switchElement = document.querySelector('#switch');
 const switchElement2 = document.querySelector('#switch2');
 const body = document.querySelector('body');
 const logoImg = document.getElementById('logo-img');
+console.log('before', logoImg.src);
 
 function darkmode() {
   if (switchElement.checked || switchElement.checked2) {
     localStorage.setItem('darkmode', 'true');
     body.classList.add('dark-theme');
-    logoImg.src = '../../src/img/header/logo-dark-theme.svg';
+    logoImg.src = './img/logo-dark-theme.svg';
+    console.log('dark', logoImg.src);
   } else {
     localStorage.setItem('darkmode', 'false');
     body.classList.remove('dark-theme');
-    logoImg.src = '/img/header/logo.svg';
+    logoImg.src = './img/header/logo.svg';
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const wasDarkMode = localStorage.getItem('darkmode') === 'true';
+  console.log('load', logoImg.src);
 
   if (wasDarkMode) {
     switchElement.checked = true;
