295c7c870b09ceef431d451389fcf525f02451e7
TymQa <125615116+TymQa@users.noreply.github.com>
2023-08-20 19:19:00 +0300

    fix bug logo

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -2,16 +2,19 @@ const switchElement = document.querySelector('#switch');
 const switchElement2 = document.querySelector('#switch2');
 const body = document.querySelector('body');
 const logoImg = document.getElementById('logo-img');
+const logoImgDark = document.getElementById('logo-img-dark');
 
 function darkmode() {
   if (switchElement.checked || switchElement.checked2) {
     localStorage.setItem('darkmode', 'true');
     body.classList.add('dark-theme');
-    logoImg.src = './img/header/logo-dark-theme.svg';
+    logoImg.style.display = 'none';
+    logoImgDark.style.display = 'block'; 
   } else {
     localStorage.setItem('darkmode', 'false');
     body.classList.remove('dark-theme');
-    logoImg.src = './img/header/logo.svg';
+    logoImg.style.display = 'block'; 
+    logoImgDark.style.display = 'none'; 
   }
 }
 
